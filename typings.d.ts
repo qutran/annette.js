fc4683f5d0a22d32901d014e0c3c507c5e83d274
qutran <kutran.dmitry@gmail.com>
2019-09-20 02:24:40 +0300

    Add typings

---
+++ typings.d.ts
@@ -0,0 +1,31 @@
+type TSubscribe = (subscribeCallback: (value: any) => void) => void;
+type TUnsubscribe = () => void;
+
+interface ISource {
+  subscribe: TSubscribe;
+}
+
+interface IExtendedSource extends ISource {
+  catch: (error: any) => { subscribe: TSubscribe };
+}
+
+type IOperatorArgs = Partial<{
+  value: any;
+  next: (value: any) => void;
+  error: (errorValue: any) => void;
+}>;
+
+type IOperator = (args: IOperatorArgs) => TUnsubscribe;
+
+export function pipe(
+  source: ISource,
+  ...operators: IOperator[]
+): IExtendedSource;
+
+export function map(callback: (value: any) => any): IOperator;
+export function filter(callback: (value: any) => any): IOperator;
+export function promise(callback: (value: any) => Promise<any>): IOperator;
+export function effect(callback: (value: any) => any): IOperator;
+export function stream(callback: (value: any) => ISource): IOperator;
+export function timeout(ms: number, onTick: (value: any) => any): IOperator;
+export function interval(ms: number, onTick: (value: any) => any): IOperator;
